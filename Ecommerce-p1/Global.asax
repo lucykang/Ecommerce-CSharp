cc640746934612dedd243a33fa26f211896bed5e
Lucy Kang <HaeYeon.Kang@MyGeorgian.ca>
2016-07-30 14:13:02 -0400

    Add project files.

---
+++ Ecommerce-p1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ecommerce_p1.MvcApplication" Language="C#" %>
---
+++ Ecommerce-p1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ecommerce_p1.Startup))]
+namespace Ecommerce_p1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
