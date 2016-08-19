8d3ddfa198c846ea47e4e3d0781f782229ad266f
Jujunol <jujunol@outlook.com>
2016-08-19 18:22:49 -0400

    Checkout and confirmation - order creation buggy

--- Ecommerce-p1/Controllers/CheckoutController.cs
+++ Ecommerce-p1/Controllers/CheckoutController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ecommerce_p1.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,7 +10,11 @@ namespace Ecommerce_p1.Controllers
     [Authorize]
     public class CheckoutController : Controller
     {
+
+        private StoreContext db = new StoreContext();
+
         // GET: Checkout
+        [HttpGet]
         public ActionResult Index()
         {
             if(!Request.IsAuthenticated)
@@ -20,9 +25,83 @@ namespace Ecommerce_p1.Controllers
         }
 
         // GET: Checkout/Confirm
+        //[HttpPost]
         public ActionResult Confirm()
         {
-            return View();
+            var cart = ShoppingCart.GetCart(HttpContext);
+
+            var model = new CartOrderViewModel()
+            {
+                CartItems = cart.GetCartItems(),
+                CartTotal = cart.GetTotal(),
+
+                Email = Request.Form["email"],
+                Name = Request.Form["name"],
+                Street = Request.Form["street"],
+                Apt = Request.Form["st2"],
+                City = Request.Form["city"],
+                Province = Request.Form["state"],
+                Postal = Request.Form["zip"],
+                Phone = Request.Form["Phonecode"] + Request.Form["phone"],
+                Country = Request.Form["country"],
+                PaymentMethod = Request.Form["paymentType"]
+            };
+
+            return View(model);
+        }
+
+        // GET: Checkout/Purchase
+        public ActionResult Purchase(CartOrderViewModel orderModel)
+        {
+            //Order order = new Order()
+            //{
+            //    Firstname = orderModel.Name,
+            //    Lastname = orderModel.Name,
+            //    Address = orderModel.Street,
+            //    City = orderModel.City,
+            //    Country = orderModel.Country,
+            //    Email = orderModel.Email,
+            //    Phone = orderModel.Phone,
+            //    PostalCode = orderModel.Postal,
+            //    Province = orderModel.Province,
+            //    Total = orderModel.CartTotal,
+            //    Username = User.Identity.Name,
+            //    OrderDate = DateTime.Now
+            //};
+
+
+            //// save the order
+            //db.Orders.Add(order);
+            ////db.SaveChanges();
+
+            //// process the order
+            //var cart = ShoppingCart.GetCart(this.HttpContext);
+            //cart.CreateOrder(order);
+
+            //return RedirectToAction("Complete", new { id = order.OrderId });
+            return RedirectToAction("Complete");
+        }
+
+        // GET: Checkout/Complete
+        public ActionResult Complete(int? id)
+        {
+            if(id == null)
+            {
+                return View(224);
+            }
+            // Validate customer owns this order
+            bool isValid = db.Orders.Any(
+                o => o.OrderId == id &&
+                o.Username == User.Identity.Name);
+
+            if (isValid)
+            {
+                return View(id);
+            }
+            else
+            {
+                return View("Error");
+            }
         }
     }
 }
---
+++ Ecommerce-p1/Models/CartOrderViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ecommerce_p1.Models
+{
+    public class CartOrderViewModel : CartViewModel
+    {
+
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Street { get; set; }
+        public string Apt { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string Postal { get; set; }
+        public string Phone { get; set; }
+        public string Country { get; set; }
+        public string PaymentMethod { get; set; }
+
+    }
+}
