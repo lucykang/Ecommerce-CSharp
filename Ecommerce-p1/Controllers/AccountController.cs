c70b83366cb1b71b4c11db4be693f2e30fb9ce72
Lucy Kang <HaeYeon.Kang@MyGeorgian.ca>
2016-08-19 14:29:33 -0400

    Added Context to the account controller

--- Ecommerce-p1/Controllers/AccountController.cs
+++ Ecommerce-p1/Controllers/AccountController.cs
@@ -17,9 +17,11 @@ namespace Ecommerce_p1.Controllers
     {
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
+        ApplicationDbContext context;
 
         public AccountController()
         {
+            context = new ApplicationDbContext();
         }
 
         public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
